1c643171abfbdccb585185db05fb59ee4bfcd813
starsinc1708 <starsinc12@yandex.ru>
2025-03-11 19:38:43 +0400

    Initial commit

---
+++ cmd/api-gateway/main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ApiGateway struct {
+		Port     int    `yaml:"port"`
+		Endpoint string `yaml:"endpoint"`
+	} `yaml:"api-gateway"`
+	BotModules map[string]struct {
+		Grpc struct {
+			Host string `yaml:"host"`
+			Port int    `yaml:"port"`
+		} `yaml:"grpc"`
+		AllowedUpdates map[string][]string `yaml:"allowed-updates"`
+	} `yaml:"bot-modules"`
+	Logs struct {
+		Address string `yaml:"address"`
+	} `yaml:"where-to-send-logs"`
+	Metrics struct {
+		Address string `yaml:"address"`
+	} `yaml:"where-to-send-metrics"`
+}
+
+var (
+	config Config
+	logger *zap.Logger
+
+	// Счётчик всех запросов
+	requestCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "api_gateway_requests_total",
+			Help: "Total number of received requests",
+		},
+	)
+
+	// Гистограмма запросов по типу чата
+	requestsByChatType = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "api_gateway_requests_by_chat_type",
+			Help:    "Requests per second by chat type",
+			Buckets: prometheus.ExponentialBuckets(1, 2, 10),
+		},
+		[]string{"chat_type"},
+	)
+
+	// Гистограмма запросов по типу обновления
+	requestsByUpdateType = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "api_gateway_requests_by_update_type",
+			Help:    "Requests per second by update type",
+			Buckets: prometheus.ExponentialBuckets(1, 2, 10),
+		},
+		[]string{"update_type"},
+	)
+)
+
+type UpdateSource string
+
+const (
+	UpdateSourceBusinessAccount UpdateSource = "BusinessAccount"
+	UpdateSourceChannel         UpdateSource = "Channel"
+	UpdateSourceGroup           UpdateSource = "Group"
+	UpdateSourceSuperGroup      UpdateSource = "SuperGroup"
+	UpdateSourcePrivateChat     UpdateSource = "PrivateChat"
+	UpdateSourceInlineMode      UpdateSource = "InlineMode"
+	UpdateSourcePoll            UpdateSource = "Poll"
+	UpdateSourcePayment         UpdateSource = "Payment"
+	UpdateSourceUnknown         UpdateSource = "Unknown"
+)
+
+type ChatType string
+
+const (
+	ChatTypePrivate    ChatType = "private"
+	ChatTypeGroup      ChatType = "group"
+	ChatTypeSupergroup ChatType = "supergroup"
+	ChatTypeChannel    ChatType = "channel"
+)
+
+func init() {
+	file, err := os.ReadFile("gateway-conf.yml")
+	if err != nil {
+		log.Fatalf("Failed to read config file: %v", err)
+	}
+	if err := yaml.Unmarshal(file, &config); err != nil {
+		log.Fatalf("Failed to parse config file: %v", err)
+	}
+
+	logger, _ = zap.NewProduction()
+	defer logger.Sync()
+
+	prometheus.MustRegister(requestCount, requestsByChatType, requestsByUpdateType)
+}
+
+func main() {
+	http.HandleFunc("/"+config.ApiGateway.Endpoint, handleUpdate)
+	http.Handle("/metrics", promhttp.Handler())
+
+	addr := fmt.Sprintf(":%d", config.ApiGateway.Port)
+	logger.Info("Starting API Gateway", zap.String("port", strconv.Itoa(config.ApiGateway.Port)))
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+	requestCount.Inc()
+
+	var update map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	logger.Info("Received update", zap.Any("update", update))
+
+	chatType, ok := extractChatType(update)
+	if !ok {
+		http.Error(w, "Unknown chat type", http.StatusBadRequest)
+		return
+	}
+
+	updateType, ok := extractUpdateType(update)
+	if !ok {
+		http.Error(w, "Unknown update type", http.StatusBadRequest)
+		return
+	}
+
+	requestsByChatType.WithLabelValues(chatType).Observe(1)
+	requestsByUpdateType.WithLabelValues(updateType).Observe(1)
+
+	dispatchUpdate(chatType, updateType, update)
+}
+
+func extractChatType(update map[string]interface{}) (string, bool) {
+	if msg, ok := update["message"].(map[string]interface{}); ok {
+		if chat, ok := msg["chat"].(map[string]interface{}); ok {
+			if chatType, ok := chat["type"].(string); ok {
+				return chatType, true
+			}
+		}
+	}
+	return "", false
+}
+
+func extractUpdateType(update map[string]interface{}) (string, bool) {
+	for key := range update {
+		if key != "update_id" {
+			return key, true
+		}
+	}
+	return "", false
+}
+
+func dispatchUpdate(chatType, updateType string, update map[string]interface{}) {
+	for module, settings := range config.BotModules {
+		if updates, ok := settings.AllowedUpdates[chatType]; ok {
+			for _, allowedUpdate := range updates {
+				if allowedUpdate == updateType {
+					go sendToModule(module, settings.Grpc.Host, settings.Grpc.Port, update)
+					return
+				}
+			}
+		}
+	}
+}
+
+func sendToModule(module, host string, port int, update map[string]interface{}) {
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", host, port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logger.Error("Failed to connect to module", zap.String("module", module), zap.Error(err))
+		return
+	}
+	defer conn.Close()
+
+	logger.Info("Sent update to module", zap.String("module", module))
+}
---
+++ go.mod
@@ -0,0 +1,26 @@
+module api-gateway
+
+go 1.24.0
+
+require (
+	github.com/prometheus/client_golang v1.21.1
+	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.71.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
+)
