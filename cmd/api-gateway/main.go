0aedfa980c6a44bda2039ba0631bd1dbef9f7f76
starsinc1708 <starsinc12@yandex.ru>
2025-03-12 14:36:24 +0400

    extract update_type

--- cmd/api-gateway/main.go
+++ cmd/api-gateway/main.go
@@ -4,15 +4,17 @@ import (
 	telegram_api "api-gateway/internal/generated/telegram-api"
 	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type Config struct {
@@ -44,15 +46,6 @@ var (
 			Name: "api_gateway_requests_total",
 			Help: "Total number of received requests",
 		},
-	)
-
-	requestsByChatType = prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name:    "api_gateway_requests_by_chat_type",
-			Help:    "Requests per second by chat type",
-			Buckets: prometheus.ExponentialBuckets(1, 2, 10),
-		},
-		[]string{"chat_type"},
 	)
 
 	requestsByUpdateType = prometheus.NewHistogramVec(
@@ -100,7 +93,7 @@ func init() {
 	logger, _ = zap.NewProduction()
 	defer logger.Sync()
 
-	prometheus.MustRegister(requestCount, requestsByChatType, requestsByUpdateType)
+	prometheus.MustRegister(requestCount, requestsByUpdateType)
 }
 
 func main() {
@@ -113,51 +106,47 @@ func main() {
 }
 
 func handleUpdate(w http.ResponseWriter, r *http.Request) {
-	var update telegram_api.Update
+	requestCount.Inc()
 
+	var update telegram_api.Update
 	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
 		http.Error(w, "Invalid request", http.StatusBadRequest)
-		logger.Error("Failed to decode JSON", zap.Error(err))
 		return
 	}
 
-	// Логируем полученный update
-	logger.Info("Received update", zap.Any("update", update))
+	updateType, ok := extractUpdateType(update)
+	if !ok {
+		http.Error(w, "Unknown chat type", http.StatusBadRequest)
+		return
+	}
+
+	logger.Info("Received update", zap.Any("update_type", updateType))
+
+	requestsByUpdateType.WithLabelValues(updateType).Observe(1)
+
 }
 
-func extractChatType(update map[string]interface{}) (string, bool) {
-	if msg, ok := update["message"].(map[string]interface{}); ok {
-		if chat, ok := msg["chat"].(map[string]interface{}); ok {
-			if chatType, ok := chat["type"].(string); ok {
-				return chatType, true
+func extractUpdateType(update telegram_api.Update) (string, bool) {
+	v := reflect.ValueOf(update)
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if field.Kind() != reflect.Pointer {
+			continue
+		}
+		if !field.IsNil() {
+			fieldName := v.Type().Field(i).Name
+			if fieldName != "" {
+				return toSnakeCase(fieldName), true
 			}
 		}
 	}
 	return "", false
 }
 
-func extractUpdateType(update map[string]interface{}) (string, bool) {
-	for key := range update {
-		if key != "update_id" {
-			return key, true
-		}
-	}
-	return "", false
-}
-
-func dispatchUpdate(chatType, updateType string, update map[string]interface{}) {
-	for module, settings := range config.BotModules {
-		if updates, ok := settings.AllowedUpdates[chatType]; ok {
-			for _, allowedUpdate := range updates {
-				if allowedUpdate == updateType {
-					go sendToModule(module)
-					return
-				}
-			}
-		}
-	}
-}
-
-func sendToModule(module string) {
-	logger.Info("Sent update to module", zap.String("module", module))
+func toSnakeCase(str string) string {
+	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
 }
